1d66826206d24237c039690bc7981e052893d616
lucas-perata <contacto.perata@gmail.com>
2023-03-10 00:28:22 -0300

    Add DB context and connection to DB with Migration

---
+++ SwimBikeRunGroopWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SwimBikeRunGroopWebApp.Models;
+
+namespace SwimBikeRunGroopWebApp.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Training> Trainings { get; set; }
+    }
+}

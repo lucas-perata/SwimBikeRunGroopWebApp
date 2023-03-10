3be818beb1a84cd36cf191067a5fce697fa1b743
lucas-perata <contacto.perata@gmail.com>
2023-03-10 20:47:24 -0300

    RACE controller and views to Detail and Index

--- SwimBikeRunGroopWebApp/Controllers/RaceController.cs
+++ SwimBikeRunGroopWebApp/Controllers/RaceController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SwimBikeRunGroopWebApp.Data;
 
 namespace SwimBikeRunGroopWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var races = _context.Races.ToList();
+            return View(races);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var race = _context.Races.Where(r => r.Id == id).FirstOrDefault();
+            return View(race);
         }
     }
 }

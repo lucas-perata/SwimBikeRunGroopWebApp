67e99112a1b76be95534258fc79c0632cb03ac93
lucas-perata <contacto.perata@gmail.com>
2023-03-10 10:56:36 -0300

    Creating controller file and index html to: CLUB, RACE, TRAINING

---
+++ SwimBikeRunGroopWebApp/Controllers/ClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwimBikeRunGroopWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SwimBikeRunGroopWebApp/Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwimBikeRunGroopWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

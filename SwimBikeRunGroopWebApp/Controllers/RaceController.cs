1fa7c027008fa3dca9b63726304e06cde82852e6
lucas-perata <contacto.perata@gmail.com>
2023-03-11 21:22:38 -0300

    Add create to race controller and views

--- SwimBikeRunGroopWebApp/Controllers/RaceController.cs
+++ SwimBikeRunGroopWebApp/Controllers/RaceController.cs
@@ -24,5 +24,23 @@ namespace SwimBikeRunGroopWebApp.Controllers
             Race race = await _raceRepository.GetByIdAsync(id);
             return View(race);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(Race race)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(race);
+            }
+
+            _raceRepository.Add(race);
+            return RedirectToAction("Index");
+        }
     }
 }

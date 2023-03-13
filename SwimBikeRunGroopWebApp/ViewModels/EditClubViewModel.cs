7655bdcb5432453a0b9324cb449e6f8dabdf66f6
lucas-perata <contacto.perata@gmail.com>
2023-03-13 00:29:19 -0300

    Create Edit action in Club controller and EditClubViewModel

---
+++ SwimBikeRunGroopWebApp/ViewModels/EditClubViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SwimBikeRunGroopWebApp.Data.Enum;
+using SwimBikeRunGroopWebApp.Models;
+
+namespace SwimBikeRunGroopWebApp.ViewModels
+{
+    public class EditClubViewModel
+    {
+        public int ClubId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Strava { get; set; }
+        public bool IsWomensOnly { get; set; }
+        public IFormFile Image { get; set; }
+        public string? URL { get; set; }
+        public int AddressId { get; set; }
+        public Address? Address { get; set; }
+        public ClubCategory ClubCategory { get; set; }
+    }
+}

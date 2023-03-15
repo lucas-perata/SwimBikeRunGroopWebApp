8dece6caa59907d86727ea2f8ef38dc377c5d422
lucas-perata <contacto.perata@gmail.com>
2023-03-15 14:35:58 -0300

    Add City, Town, Province and IMAGEURL to AppUser model

--- SwimBikeRunGroopWebApp/Models/AppUser.cs
+++ SwimBikeRunGroopWebApp/Models/AppUser.cs
@@ -5,12 +5,16 @@ namespace SwimBikeRunGroopWebApp.Models
 {
     public class AppUser : IdentityUser
     {
-        public int Pace { get; set; }
-        public int RunWeekly { get; set; }
-        public int Swim { get; set; }
-        public int SwimWeekly { get; set; }
-        public int Bike { get; set; }
-        public int BikeWeekly { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public string? Town { get; set; }
+        public string? City { get; set; }
+        public string? Province { get; set; }
+        public int? Pace { get; set; }
+        public int? RunWeekly { get; set; }
+        public int? Swim { get; set; }
+        public int? SwimWeekly { get; set; }
+        public int? Bike { get; set; }
+        public int? BikeWeekly { get; set; }
         public int? AddressId { get; set; }
         public Address? Address { get; set; }
         public ICollection<Club> Clubs { get; set; }

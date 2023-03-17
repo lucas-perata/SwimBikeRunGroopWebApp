8be9d3ca94fa1f156713b338dbcf60dcadc24f05
lucas-perata <contacto.perata@gmail.com>
2023-03-17 10:33:15 -0300

    Models and Migration to ClubUsers table

---
+++ SwimBikeRunGroopWebApp/Models/UserClub.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SwimBikeRunGroopWebApp.Models
+{
+    public class UserClub
+    {
+        [Key]
+        public int Id { get; set; } 
+        public int ClubId { get; set; }
+        public Club Club { get; set; }
+
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}

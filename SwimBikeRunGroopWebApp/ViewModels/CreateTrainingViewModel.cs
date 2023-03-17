0f5bac1cc0137acfff8d4c6f48ddb310bfa7b7a5
lucas-perata <contacto.perata@gmail.com>
2023-03-16 21:13:12 -0300

    Add clubid of admin user to training

---
+++ SwimBikeRunGroopWebApp/ViewModels/CreateTrainingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using SwimBikeRunGroopWebApp.Data.Enum;
+using SwimBikeRunGroopWebApp.Models;
+
+namespace SwimBikeRunGroopWebApp.ViewModels
+{
+    public class CreateTrainingViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int DistanceFromStart { get; set; }
+        public string StartAddress { get; set; }
+        public int AveragePace { get; set; }
+        public string AppUserId { get; set; }
+        public SportCategory SportCategory { get; set; }
+
+        public Club? Club { get; set; }
+        public int? ClubId { get; set; }
+   
+    }
+}
